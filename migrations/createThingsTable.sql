52be3d83fc9a42f574fdf3f77d7d595fe77361a7
AGadoev <AGadoev@croc.ru>
2019-09-18 17:36:24 +0300

    start

---
+++ migrations/createThingsTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE things (
+    vendorId text,
+    title text,
+    color text,
+    size text,
+    barcode bigint primary key,
+    oldPrice int,
+    newPrice int,
+    balance int,
+    brand text
+);
