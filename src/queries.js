f697f4ad30429edb7c73df030e106481585ed231
AGadoev <AGadoev@croc.ru>
2019-10-02 00:09:36 +0300

    need async constructor

--- src/queries.js
+++ src/queries.js
@@ -32,42 +32,56 @@ async function getAvailableColors (vendorcode) {
   }
 }
 
-async function getThingByBarcode(request, response) {
+const getThingByBarcode = async (request, response) => {
   const barcode = request.params.barcode.toString();
-  //получаем всю инфу о вещи из бд
-  let thing = await db.pool.query(`SELECT * FROM ${db.table} WHERE bar_code = ${barcode}`);
-  thing = thing.rows[0]; // в thing хранится json объект со всеми полями таблицы базы данных
-  const thingParams = {
-    title: thing.title,
-    vendorcode: thing.vendor_code,
-    price: thing.price,
-    size: thing.size,
-    brand: thing.brand,
-    color: thing.color,
-    barcode: thing.bar_code,
-  };
+  const thing = new Thing(barcode);
+  thing.getInfoFromDB()
+  response.status(200).json(thing)
+};
 
-  const thingObject = new Thing(thingParams); // создаем объект вещи
+// async function getThingByBarcode(request, response) {
 
-  // добавляем размеры к вещи
-  let availableSizes = await getAvailableSizes(thingObject.vendorcode); 
-  thingObject.availableSizes = availableSizes;
+//   /*
+//     const thing = new Thing ('barcode');
+//     response.status(200).json(thing)
+//   */
 
-  // добавляем цвета к вещи
-  let availableColors = await getAvailableColors(thingObject.vendorcode); 
-  thingObject.availableColors = availableColors;
+//   const barcode = request.params.barcode.toString();
+//   //получаем всю инфу о вещи из бд
+//   let thing = await db.pool.query(`SELECT * FROM ${db.table} WHERE bar_code = ${barcode}`);
+//   thing = thing.rows[0]; // в thing хранится json объект со всеми полями таблицы базы данных
+//   const thingParams = {
+//     title: thing.title,
+//     vendorcode: thing.vendor_code,
+//     price: thing.price,
+//     size: thing.size,
+//     brand: thing.brand,
+//     color: thing.color,
+//     barcode: thing.bar_code,
+//   };
 
-  if (fs.existsSync(`../public/images/${thingObject.vendorcode}-01.jpg`)) { // если есть фото
-    var imageAsBase64 = fs.readFileSync(`../public/images/${thingObject.vendorcode}-01.jpg`, 'base64');
-    console.log(imageAsBase64);
-  } else {
-    throw new Error ('нет фотографии')
-  }
+//   const thingObject = new Thing(thingParams); // создаем объект вещи
 
-  response.status(200).json(thingObject); // отправляем готовый объект шмотки
-  // // how to convert JPG to BASE64
-  // // var imageAsBase64 = fs.readFileSync('../public/images/sea.jpg', 'base64');
-}
+//   // добавляем размеры к вещи
+//   let availableSizes = await getAvailableSizes(thingObject.vendorcode); 
+//   thingObject.availableSizes = availableSizes;
+
+//   // добавляем цвета к вещи 
+//   let availableColors = await getAvailableColors(thingObject.vendorcode); 
+//   thingObject.availableColors = availableColors;
+
+//   if (fs.existsSync(`../public/images/${thingObject.vendorcode}-01.jpg`)) { // если есть фото
+//     const imageAsBase64 = fs.readFileSync(`../public/images/${thingObject.vendorcode}-01.jpg`, 'base64');
+//     thingObject._img_base64 = imageAsBase64;
+//   } else {
+//     console.log(`нет фотографии для вещи ${thingObject.barcode}`);
+//     const imageAsBase64 = fs.readFileSync(`../public/no_foto.png`, 'base64');
+//     thingObject._img_base64 = imageAsBase64;
+//   }
+
+//   response.status(200).json(thingObject); // отправляем готовый объект шмотки
+// }
+
 module.exports = {
   getThingByBarcode,
 };
